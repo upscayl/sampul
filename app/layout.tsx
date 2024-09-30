d34d506ad08c36e26be27ffc5e029f8e71d32cff
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2024-09-30 15:46:14 +0530

    Update SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,21 @@ const font = Font({
 export const metadata: Metadata = {
   title: "Sampul - Custom Resolution Image Samples",
   description: "Custom Resolution Image Samples",
+  openGraph: {
+    url: "https://sampul.upscayl.org/",
+    description: "Custom Resolution Image Samples",
+    siteName: "Sampul",
+    images: [
+      {
+        url: "https://sampul.upscayl.org/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Sampul - Custom Resolution Image Samples",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
